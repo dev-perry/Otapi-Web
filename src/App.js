de3b9fa12c90c6674371d4f915f5318ac5975cfe
perry <pasibeybonsu@drew.edu>
2020-05-19 22:16:04 -0400

    Product view path configured

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/navigation.js';
 import Home from './components/home.js';
 import Products from './components/products.js';
 import CartPopover from './components/cart_popover.js';
+import ProductView from './components/product_view.js';
 
 function App() {
   return (
@@ -11,11 +12,12 @@ function App() {
     <CartPopover/>
     <Navigation>
       <Switch>
+        <Route path="/product/:id" render={(props) => <ProductView {...props}/>}/>
         <Route path="/living"><Products category="living"/></Route>
         <Route path="/prints"><Products category="prints"/></Route>
         <Route path="/accessories"><Products category="accessories"/></Route>
         <Route path="/apparel"><Products category="apparel"/></Route>
-        <Route path="/"><Home/></Route>
+        <Route exact path="/"><Home/></Route>
       </Switch>
   </Navigation>
   </>
